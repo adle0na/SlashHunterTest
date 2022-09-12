797762ce16be81aa4a26131604e675754bd1a631
Adle0na <uuuuufgh@gmail.com>
2022-09-13 02:28:21 +0900

    [Save] 대쉬 기능완료 쿨타임 설정중

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,44 +1,44 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]
-    private float moveSpeed = 3f;
-
+    private float moveSpeed = 2f;
     private float activeMoveSpeed;
-    private float dashSpeed;
-    private float dashLength   = 0.5f;
-    private float dashCooldown = 1f;
-    private float dashCounter;
-    private float dashCoolCounter;
-
+    private bool  canDash    = true;
+    private float dashSpeed  = 8f;
+    private float dashLength = 0.1f;
+    private float dashCooltime = 2f;
+    
     public JoyStickControl joyStickControl; 
     
     private Rigidbody2D rigid2d;
     private Vector3     movementVector;
     private Animator    anim;
     
+    [SerializeField]
+    private Cooldown _cooldown;
+    
     [Header("캐릭터")]
     [SerializeField]
     private SpriteRenderer _spriteRenderer;
-
-    [SerializeField]
-    private TrailRenderer  _trailRenderer;
-
- 
-
+    
     private void Awake()
     {
         activeMoveSpeed = moveSpeed;
-        
+
         rigid2d = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         anim = GetComponentInChildren<Animator>();
         movementVector = new Vector3();
+        
+        // 값 초기화, 쿨타임 숫자 비활성화
+        _cooldown.textCooldown.gameObject.SetActive(false);
+        _cooldown.imageCooldown.fillAmount = 0.0f;
     }
 
     private void FixedUpdate()
@@ -46,10 +46,10 @@ public class PlayerMovement : MonoBehaviour
         // 조이스틱 조작
         if (joyStickControl.joystickVec.y != 0)
         {
-            rigid2d.velocity = new Vector2(joyStickControl.joystickVec.x * moveSpeed,
-                                           joyStickControl.joystickVec.y * moveSpeed);
-            // 움직임 실행 코루틴
-            StartCoroutine("HorizontalCheck");
+            rigid2d.velocity = new Vector2(joyStickControl.joystickVec.x * activeMoveSpeed,
+                                           joyStickControl.joystickVec.y * activeMoveSpeed);
+  
+            _spriteRenderer.flipX = joyStickControl.joystickVec.x < 0;
             anim.SetBool("isMoving", true);
         }
         else // 초기화
@@ -60,15 +60,38 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private IEnumerator HorizontalCheck()
+    public void Dash()
     {
-        // 조이스틱 x벡터값으로 좌우 확인
-        if (joyStickControl.joystickVec.x < 0)
-            _spriteRenderer.flipX = true;
-        else
-            _spriteRenderer.flipX = false;
+        if (canDash)
+            StartCoroutine(DashActivate());
+    }
+    
+    private IEnumerator DashActivate()
+    {
+        // 이동속도 대쉬 속도
+        activeMoveSpeed = dashSpeed;
+
+        // 쿨타임 가시화
+        _cooldown.isCooldown = true;
+        _cooldown.textCooldown.gameObject.SetActive(true);
+        _cooldown.cooldownTimer = _cooldown.cooldownTime;
         
-        yield break;
+        // 대쉬 애니메이션 실행 후 쿨타임 실행
+        anim.SetBool("isSliding", true);
+        canDash = false;
+        yield return new WaitForSeconds(dashLength);
+        
+        // 대쉬 애니메이션 종료 후 일반 속도
+        anim.SetBool("isSliding", false);
+        activeMoveSpeed = moveSpeed;
+
+        // 대쉬 쿨타임 실행
+        yield return new WaitForSeconds(dashCooltime);
+        
+        // 대쉬 초기화
+        canDash = true;
     }
     
 }
+
+
